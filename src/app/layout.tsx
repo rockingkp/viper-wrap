56e1b95eb7002b31e58107596df89e4da9f0eb58
rockingkp <kaamyakpant@gmail.com>
2024-07-13 23:09:00 +0530

    implement file drag drop

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar /> {children}
-        <Footer />
+        <Navbar />
+
+        <main className="flex flex-col min-h-[calc(100vh-3.5rem-1px)]">
+          <div className="flex-1 flex flex-col h-full">{children}</div>
+          <Footer />
+        </main>
       </body>
     </html>
   );
