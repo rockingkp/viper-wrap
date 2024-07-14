ad26925e048b21e8736ed8cb4e409c3d16123c51
rockingkp <kaamyakpant@gmail.com>
2024-07-14 17:41:48 +0530

    implement colour customization phone case

--- src/app/configure/design/DesignConfigurator.tsx
+++ src/app/configure/design/DesignConfigurator.tsx
@@ -2,9 +2,14 @@
 
 import HandleComponent from "@/components/HandleComponent";
 import { AspectRatio } from "@/components/ui/aspect-ratio";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { cn } from "@/lib/utils";
 import NextImage from "next/image";
 import { Rnd } from "react-rnd";
+import { Radio, RadioGroup } from "@headlessui/react";
+import { useState } from "react";
+import { COLORS } from "@/validators/option-validator";
+import { Label } from "@/components/ui/label";
 
 interface DesignConfiguratorProps {
   configId: string;
@@ -17,6 +22,10 @@ const DesignConfigurator = ({
   imageUrl,
   imageDimensions,
 }: DesignConfiguratorProps) => {
+  const [options, setOptions] = useState<{ color: (typeof COLORS)[number] }>({
+    color: COLORS[0],
+  });
+
   return (
     <div className="relative mt-20 grid grid-cols-3 mb-20 pb-20">
       <div className="relative h-[37.5rem] overflow-hidden col-span-2 w-full max-w-4xl flex items-center justify-center rounded-lg border-2 border-dashed border-gray-300 p-12 text-center focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 ">
@@ -36,7 +45,7 @@ const DesignConfigurator = ({
           <div
             className={cn(
               "absolute inset-0 left-[3px] top-px right-[3px] bottom-px rounded-[32px] ",
-              `bg-blue-950`
+              `bg-${options.color.tw}`
             )}
           />
         </div>
@@ -65,6 +74,61 @@ const DesignConfigurator = ({
             />
           </div>
         </Rnd>
+      </div>
+
+      {/* Configuration options */}
+      <div className="h-[37.5rem] flex flex-col bg-white">
+        <ScrollArea className="relative flex-1 overflow-auto">
+          <div
+            aria-hidden="true"
+            className="absolute z-10 inset-x-0 bottom-0 h-12 bg-gradient-to-t from-white pointer-events-none"
+          />
+
+          <div className="px-8 pb-12 pt-8">
+            <h2 className="tracking-tight font-bold text-3xl">
+              Customize your case
+            </h2>
+
+            <div className="w-full h-px bg-zinc-200 my-6" />
+
+            <div className="relative mt-4 h-full flex flex-col justify-between">
+              <RadioGroup
+                value={options.color}
+                onChange={(val) => {
+                  setOptions((prev) => ({
+                    ...prev,
+                    color: val,
+                  }));
+                }}
+              >
+                <Label>Colour: {options.color.label}</Label>
+                <div className="mt-3 flex items-center space-x-3">
+                  {COLORS.map((color) => (
+                    <Radio
+                      key={color.label}
+                      value={color}
+                      className={({ focus, checked }) =>
+                        cn(
+                          "relative -m-0.5 flex cursor-pointer items-center justify-center rounded-full p-0.5 active:ring-0 focus:ring-0 active:outline-none focus:outline-none border-2 border-transparent",
+                          {
+                            [`border-${color.tw}`]: focus || checked,
+                          }
+                        )
+                      }
+                    >
+                      <span
+                        className={cn(
+                          `bg-${color.tw}`,
+                          "h-8 w-8 rounded-full border border-black border-opacity-10"
+                        )}
+                      />
+                    </Radio>
+                  ))}
+                </div>
+              </RadioGroup>
+            </div>
+          </div>
+        </ScrollArea>
       </div>
     </div>
   );
