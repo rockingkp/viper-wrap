3fc59d7167d3b488859d1e50bf1060ed92065a64
rockingkp <kaamyakpant@gmail.com>
2024-07-18 02:49:51 +0530

    implement thank you customised email through react-email and resend

--- src/app/api/webhooks/route.ts
+++ src/app/api/webhooks/route.ts
@@ -3,6 +3,10 @@ import { headers } from "next/headers";
 import { db } from "@/db";
 import { NextResponse } from "next/server";
 import Stripe from "stripe";
+import { Resend } from "resend";
+import OrderReceivedEmail from "@/components/emails/OrderReceivedEmail";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: Request) {
   try {
@@ -37,7 +41,7 @@ export async function POST(req: Request) {
     }
 
     const billingAddress = session.customer_details!.address;
-    //@ts-ignore
+    // @ts-ignore
     const shippingAddress = session.shipping!.address;
 
     const updatedOrder = await db.order.update({
@@ -67,6 +71,26 @@ export async function POST(req: Request) {
           },
         },
       },
+    });
+
+    await resend.emails.send({
+      from: "ViperWrap <kaamyakpant@gmail.com>",
+      // @ts-ignore
+      to: [event.data.object.customer_details.email],
+      subject: "Thanks for your order!",
+      react: OrderReceivedEmail({
+        orderId,
+        orderDate: updatedOrder.createdAt.toLocaleDateString(),
+        // @ts-ignore
+        shippingAddress: {
+          name: session.customer_details!.name!,
+          city: shippingAddress!.city!,
+          country: shippingAddress!.country!,
+          postalCode: shippingAddress!.postal_code!,
+          street: shippingAddress!.line1!,
+          state: shippingAddress!.state!,
+        },
+      }),
     });
 
     return NextResponse.json({ result: event, ok: true });
