767f752e790e6e906311407a842f1bcfd10d8673
rockingkp <kaamyakpant@gmail.com>
2024-07-18 03:48:21 +0530

    fixes and metadata for the app

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,13 +5,11 @@ import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
 import { Toaster } from "@/components/ui/toaster";
 import Providers from "@/components/Providers";
+import { constructMetadata } from "@/lib/utils";
 
 const recursive = Recursive({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Viper Wrap",
-  description: "Complete E-commerce shop for your own customised phone case",
-};
+export const metadata = constructMetadata();
 
 export default function RootLayout({
   children,
